55c40b51e55b85d0c7ba9a776bbfd576be0c5555
GuySchnidrig <78217667+GuySchnidrig@users.noreply.github.com>
2023-10-05 00:00:39 +0200

    update_MI_fast

--- Run_Server.R
+++ Run_Server.R
@@ -1,2 +1,3 @@
 # Run server in console
-rsconnect::deployApp('cdmr_stats_server.rmd')
+setwd("G:/Meine Ablage/MTG/cmdr_stats")
+rsconnect::deployApp('cdmr_stats_server.rmd', launch.browser = FALSE)

9d8f64ddaf162b15c8b778f86b37cd96e4d1657d
GuySchnidrig <78217667+GuySchnidrig@users.noreply.github.com>
2024-03-18 16:41:44 +0100

    working time and deck name

--- cmdr tracker/tracker.py
+++ cmdr tracker/tracker.py
@@ -6,44 +6,45 @@ app = Flask(__name__)
 
 # Initial life totals for 4 players
 players_life = {'Player 1': 40, 'Player 2': 40, 'Player 3': 40, 'Player 4': 40}
-players_time = {'Player 1': 0, 'Plaer 2': 0, 'Player 3': 0, 'Player 4': 0}
+players_time = {'Player 1': 0, 'Player 2': 0, 'Player 3': 0, 'Player 4': 0}
 
-# Initial active player and turn count
+# Initial active player, turn count, start_time, turn_time, start_turn_time
 active_player_index = 0
 turn_count = 1
 start_time = None
 turn_time = None
 start_turn_time = None
 
-
 @app.route('/')
-def index():
-    active_player = list(players_life.keys())[active_player_index]
+def index():    
     return render_template('enter_players.html')
 
 @app.route('/submit_players', methods=['POST'])
 def submit_players():
-    global players_life, start_time, turn_time, start_turn_time, turn_times_df, player_names, active_player_index
+    global players_life, start_time, turn_time, start_turn_time, turn_times_df, player_names, active_player_index, players_time, deck_names
     num_players = int(request.form['num_players'])
     players_life = {request.form[f'player{i+1}']: 40 for i in range(num_players)}
-    
+    players_time = {request.form[f'player{i+1}']: 0 for i in range(num_players)}
+
     # Get the list of player names from the form inputs
     player_names = [request.form[f'player{i+1}'] for i in range(num_players)]
+    deck_names = [request.form[f'deck{i+1}'] for i in range(num_players)]
     
-    # Create a pandas DataFrame to store turn times per player
-    turn_times_df = pd.DataFrame(0, index=['turn time'], columns=[active_player_index])
-    
-    # time
+    # Update time
     start_time = time.time()
     turn_time = time.time()
     start_turn_time = time.time()
+    
+    #  Elapsed_time
     end_time = time.time()
     elapsed_time = end_time - start_time
-    return render_template('index.html', players=players_life, active_player=list(players_life.keys())[active_player_index], turn_count=turn_count, elapsed_time=elapsed_time, turn_time=turn_time, turn_times_df = turn_times_df, player_names = player_names, active_player_index = active_player_index)
+    
+    return render_template('index.html', players=players_life, active_player=list(players_life.keys())[active_player_index], turn_count=turn_count, elapsed_time=elapsed_time,
+                           turn_time=turn_time, players_time = players_time, player_names = player_names, active_player_index = active_player_index, deck_names = deck_names)
 
 @app.route('/update_life', methods=['POST'])
 def update_life():
-    global players_life, start_time, turn_times_df
+    global players_life, start_time, players_time, deck_names
     player = request.form['player']
     action = request.form['action']
     amount = int(request.form['amount'])
@@ -53,41 +54,63 @@ def update_life():
     elif action == 'decrease':
         players_life[player] -= amount
 
+    #  Elapsed_time
     end_time = time.time()
     elapsed_time = end_time - start_time
 
-    return render_template('index.html', players=players_life, active_player=list(players_life.keys())[active_player_index], turn_count=turn_count,  elapsed_time=elapsed_time, turn_times_df = turn_times_df, active_player_index = active_player_index)
+    return render_template('index.html', players=players_life, active_player=list(players_life.keys())[active_player_index], 
+                           turn_count=turn_count,  elapsed_time=elapsed_time, players_time = players_time, 
+                           active_player_index = active_player_index, deck_names = deck_names)
 
 @app.route('/pass_turn', methods=['POST'])
 def pass_turn():
-    global active_player_index, turn_count, start_time, start_turn_time, turn_time, elapsed_time, turn_times_df, active_player_index
-    active_player_index = (active_player_index + 1) % len(players_life)
-    end_time = time.time()
-    elapsed_time = end_time - start_time
+    global active_player_index, turn_count, start_time, start_turn_time, turn_time, elapsed_time, players_time, active_player_index, deck_names
     
+    end_time = time.time()
     turn_time = end_time - start_turn_time
     
-    # Append turn time for the active player to the DataFrame
-    turn_times_df[active_player_index] = [turn_time]
-    print(turn_times_df)
-
+    # Update players' turn times
+    active_player = list(players_life.keys())[active_player_index]
+    players_time[active_player] = players_time.get(active_player, 0) + turn_time
+    print(players_time)
+    print(players_life)
+    
+    # Update active player index
+    active_player_index = (active_player_index + 1) % len(players_life)
+    
     if active_player_index == 0:  # If it's the first player's turn again
         turn_count += 1
 
     start_turn_time = time.time()  # Start time for the next turn
-
-    return render_template('index.html', players=players_life, active_player=list(players_life.keys())[active_player_index], turn_count=turn_count, start_turn_time = start_turn_time, elapsed_time=elapsed_time, 
-                           turn_time = turn_time, turn_times_df = turn_times_df, active_player_index = active_player_index)
-
+ 
+    #  Elapsed_time
+    elapsed_time = end_time - start_time
+    
+    return render_template('index.html', players=players_life, active_player=list(players_life.keys())[active_player_index], 
+                           turn_count=turn_count, start_turn_time = start_turn_time, elapsed_time=elapsed_time, 
+                           turn_time = turn_time, players_time = players_time, active_player_index = active_player_index,
+                           deck_names = deck_names)
 
 @app.route('/change_active_player', methods=['POST'])
 def change_active_player():
-    global active_player_index, start_time, elapsed_time
+    global active_player_index, start_time, elapsed_time, players_time, deck_names
+    
+    
+    # Update players' turn times
+    end_time = time.time()
+    turn_time = end_time - start_turn_time
+    
+    #  Elapsed_time
+    elapsed_time = end_time - start_time
+    
+    active_player = list(players_life.keys())[active_player_index]
+    players_time[active_player] = players_time.get(active_player, 0) + turn_time
+    
     selected_player = request.form.get('active_player')  # Get the selected player from the form data
     active_player_index = list(players_life.keys()).index(selected_player)  # Set active player index based on selected player
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    return render_template('index.html', players=players_life, active_player=selected_player, turn_count=turn_count,  elapsed_time=elapsed_time, active_player_index = active_player_index)
+   
+    return render_template('index.html', players=players_life, active_player=selected_player, turn_count=turn_count,
+                           elapsed_time=elapsed_time, active_player_index = active_player_index, players_time = players_time, deck_names = deck_names)
 
 if __name__ == '__main__':
     app.run(debug=True)
